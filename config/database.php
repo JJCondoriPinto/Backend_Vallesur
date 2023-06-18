64595c43eee66cc0e11939036681fc934d963090
JuanJoseMCP <juanjosecondoripinto@gmail.com>
2023-06-18 17:59:11 -0500

    database server

--- config/database.php
+++ config/database.php
@@ -98,6 +98,8 @@ return [
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '27017'),
             'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
         ]
 
     ],
